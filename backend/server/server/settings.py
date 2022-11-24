a49b552d9b4fbca2db6b3cc019f28eaa0a7fa1ef
hadjigagny93 <hadjigagny93@gmail.com>
2022-11-24 19:38:17 +0100

    temp hide secret with simple .py conf file

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from .config import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&r@9#qt89wv$v(d@&+z3y)4vo+uoda(z=$7h#vzl#(x1a*2q8a'
+#SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
